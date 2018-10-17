326cb593a483adfbc12927d186e62e75ef01885e
Josh Corneille <jcorneille107@gmail.com>
2018-10-17 13:56:27 -0400

    bison compiles with no errors

--- cMinus.cpp
+++ cMinus.cpp
@@ -14,11 +14,11 @@ map<string, string> state_string;
 // preconditon: n>=0
 void output_tabs(int n)
 {
-	while (n > 0)
-	{
-		cout << "\t";
-		n--;
-	}
+    while (n > 0)
+    {
+        cout << "\t";
+        n--;
+    }
 }
 
 /* Conditions Nodes */
@@ -29,16 +29,16 @@ or_cond_node::or_cond_node(cond_node *L, cond_node *R) : left(L), right(R)
 
 void or_cond_node::print()
 {
-	cout << "(";
-	left->print();
-	cout << ") ||  (";
-	right->print();
-	cout << ")";
+    cout << "(";
+    left->print();
+    cout << ") ||  (";
+    right->print();
+    cout << ")";
 }
 
 bool or_cond_node::evaluate()
 {
-	return (left->evaluate()) || (right->evaluate());
+    return (left->evaluate()) || (right->evaluate());
 }
 
 and_cond_node::and_cond_node(cond_node *L, cond_node *R) : left(L), right(R)
@@ -47,50 +47,50 @@ and_cond_node::and_cond_node(cond_node *L, cond_node *R) : left(L), right(R)
 
 void and_cond_node::print()
 {
-	cout << "(";
-	left->print();
-	cout << ") &&  (";
-	right->print();
-	cout << ")";
+    cout << "(";
+    left->print();
+    cout << ") &&  (";
+    right->print();
+    cout << ")";
 }
 
 bool and_cond_node::evaluate()
 {
-	return (left->evaluate()) && (right->evaluate());
+    return (left->evaluate()) && (right->evaluate());
 }
 
 neg_cond_node::neg_cond_node(cond_node *child)
 {
-	this->child = child;
+    this->child = child;
 }
 
 void neg_cond_node::print()
 {
-	cout << "! (";
-	child->print();
-	cout << ")";
+    cout << "! (";
+    child->print();
+    cout << ")";
 }
 
 bool neg_cond_node::evaluate()
 {
-	return !(child->evaluate());
+    return !(child->evaluate());
 }
 
 prim_cond_node::prim_cond_node(operation op, exp_node *left, exp_node *right)
 {
-	this->op = op;
-	this->left = left;
-	this->right = right;
+    this->op = op;
+    this->left = left;
+    this->right = right;
 }
 
 void prim_cond_node::print()
 {
-	left->print();
-	switch (op)
-	{
-	case GT:
-		cout << ">";
-		break;
+    left->print();
+    switch (op)
+    {
+    case GT:
+        cout << ">";
+        break;
     case GE:
         cout << ">=";
         break;
@@ -100,44 +100,44 @@ void prim_cond_node::print()
     case LE:
         cout << "<=";
         break;
-	case EQ:
-		cout << "==";
-		break;
+    case EQ:
+        cout << "==";
+        break;
     case NO:
         cout << "!=";
         break;
-	}
-	right->print();
+    }
+    right->print();
 }
 
 bool prim_cond_node::evaluate()
 {
-	float opdl, opdr;
+    float opdl, opdr;
 
-	opdl = left->evaluate();
-	opdr = right->evaluate();
+    opdl = left->evaluate();
+    opdr = right->evaluate();
 
-	switch (op)
-	{
+    switch (op)
+    {
     case GT:
-		return (opdl > opdr);
-		break;        
-	case GE:
-		return (opdl >= opdr);
-		break;
+        return (opdl > opdr);
+        break;
+    case GE:
+        return (opdl >= opdr);
+        break;
     case LT:
-		return (opdl < opdr);
-		break;
+        return (opdl < opdr);
+        break;
     case LE:
-		return (opdl <= opdr);
-		break;
-	case EQ:
-		return (opdl == opdr);
-		break;
+        return (opdl <= opdr);
+        break;
+    case EQ:
+        return (opdl == opdr);
+        break;
     case NO:
-		return (opdl != opdr);
-		break;
-	}
+        return (opdl != opdr);
+        break;
+    }
 }
 
 /* End Condition Node */
@@ -148,35 +148,35 @@ bool prim_cond_node::evaluate()
 // and stores the character representation of the operator.
 operator_node::operator_node(exp_node *L, exp_node *R)
 {
-	left = L;
-	right = R;
+    left = L;
+    right = R;
 }
 
 number_node::number_node(float value)
 {
-	num = value;
+    num = value;
 }
 
 void number_node::print()
 {
-	cout << num;
+    cout << num;
 }
 
 float number_node::evaluate()
 {
-	return num;
+    return num;
 }
 
 id_node::id_node(string value) : id(value) {}
 
 void id_node::print()
 {
-	cout << id;
+    cout << id;
 }
 
 float id_node::evaluate()
 {
-	return state_float[id];
+    return state_float[id];
 }
 
 // plus_node inherits the characteristics of node and adds its own evaluate function
@@ -187,21 +187,21 @@ plus_node::plus_node(exp_node *L, exp_node *R) : operator_node(L, R)
 
 void plus_node::print()
 {
-	cout << "(";
-	left->print();
-	cout << " + ";
-	right->print();
-	cout << ")";
+    cout << "(";
+    left->print();
+    cout << " + ";
+    right->print();
+    cout << ")";
 }
 
 float plus_node::evaluate()
 {
-	float left_num, right_num;
+    float left_num, right_num;
 
-	left_num = left->evaluate();
-	right_num = right->evaluate();
+    left_num = left->evaluate();
+    right_num = right->evaluate();
 
-	return left_num + right_num;
+    return left_num + right_num;
 }
 
 // minus_node inherits the characteristics of node and adds its own evaluate function
@@ -211,21 +211,21 @@ minus_node::minus_node(exp_node *L, exp_node *R) : operator_node(L, R)
 
 void minus_node::print()
 {
-	cout << "(";
-	left->print();
-	cout << " - ";
-	right->print();
-	cout << ")";
+    cout << "(";
+    left->print();
+    cout << " - ";
+    right->print();
+    cout << ")";
 }
 
 float minus_node::evaluate()
 {
-	float left_num, right_num;
+    float left_num, right_num;
 
-	left_num = left->evaluate();
-	right_num = right->evaluate();
+    left_num = left->evaluate();
+    right_num = right->evaluate();
 
-	return left_num - right_num;
+    return left_num - right_num;
 }
 
 // times_node inherits the characteristics of node and adds its own evaluate function
@@ -235,21 +235,21 @@ times_node::times_node(exp_node *L, exp_node *R) : operator_node(L, R)
 
 void times_node::print()
 {
-	cout << "(";
-	left->print();
-	cout << " * ";
-	right->print();
-	cout << ")";
+    cout << "(";
+    left->print();
+    cout << " * ";
+    right->print();
+    cout << ")";
 }
 
 float times_node::evaluate()
 {
-	float left_num, right_num;
+    float left_num, right_num;
 
-	left_num = left->evaluate();
-	right_num = right->evaluate();
+    left_num = left->evaluate();
+    right_num = right->evaluate();
 
-	return left_num * right_num;
+    return left_num * right_num;
 }
 
 // divide_node inherits the characteristics of node and adds its own evaluate function
@@ -260,45 +260,45 @@ divide_node::divide_node(exp_node *L, exp_node *R) : operator_node(L, R)
 
 void divide_node::print()
 {
-	cout << "(";
-	left->print();
-	cout << " / ";
-	right->print();
-	cout << ")";
+    cout << "(";
+    left->print();
+    cout << " / ";
+    right->print();
+    cout << ")";
 }
 
 float divide_node::evaluate()
 {
-	float left_num, right_num;
+    float left_num, right_num;
 
-	left_num = left->evaluate();
-	right_num = right->evaluate();
+    left_num = left->evaluate();
+    right_num = right->evaluate();
 
-	if (right_num)
-	{
-		return left_num / right_num;
-	}
-	else
-	{
-		cout << "division by zero -> " << left_num << " / " << 0 << endl;
-		//  include stdlib.h for exit
-		exit(1);
-	}
+    if (right_num)
+    {
+        return left_num / right_num;
+    }
+    else
+    {
+        cout << "division by zero -> " << left_num << " / " << 0 << endl;
+        //  include stdlib.h for exit
+        exit(1);
+    }
 }
 
 unary_minus_node::unary_minus_node(exp_node *L) : exp(L) {}
 
 void unary_minus_node::print()
 {
-	cout << "- ( ";
-	exp->print();
-	cout << " )";
+    cout << "- ( ";
+    exp->print();
+    cout << " )";
 }
 
 float unary_minus_node::evaluate()
 {
-	float expValue = exp->evaluate();
-	return -expValue;
+    float expValue = exp->evaluate();
+    return -expValue;
 }
 
 /* End Expression Nodes */
@@ -307,114 +307,114 @@ float unary_minus_node::evaluate()
 
 ife_stmt::ife_stmt(test *condition, statement *thenbranch, statement *elsebranch)
 {
-	this->condition = condition;
-	this->thenbranch = thenbranch;
-	this->elsebranch = elsebranch;
+    this->condition = condition;
+    this->thenbranch = thenbranch;
+    this->elsebranch = elsebranch;
 }
 
 void ife_stmt::print(int n)
 {
-	output_tabs(n);
-	cout << "if (";
-	condition->print();
-	cout << ")";
-	cout << endl;
-	output_tabs(n);
-	cout << " { " << endl;
-	thenbranch->print(n + 1);
-	cout << "} " << endl;
-	output_tabs(n);
+    output_tabs(n);
+    cout << "if (";
+    condition->print();
+    cout << ")";
+    cout << endl;
+    output_tabs(n);
+    cout << " { " << endl;
+    thenbranch->print(n + 1);
+    cout << "} " << endl;
+    output_tabs(n);
 }
 
 void ife_stmt::evaluate()
 {
 
-	if (condition->evaluate())
-	{
-		thenbranch->evaluate();
-	}
-	else
-	{
-		elsebranch->evaluate();
-	}
+    if (condition->evaluate())
+    {
+        thenbranch->evaluate();
+    }
+    else
+    {
+        elsebranch->evaluate();
+    }
 }
 
 while_stmt::while_stmt(test *condition, statement *bodystmt)
 {
-	this->condition = condition;
-	this->bodystmt = bodystmt;
+    this->condition = condition;
+    this->bodystmt = bodystmt;
 }
 
 void while_stmt::print(int n)
 {
-	output_tabs(n);
-	cout << "while ";
-	condition->print();
-	cout << " do {" << endl;
-	bodystmt->print(n + 1);
-	cout << "} ";
+    output_tabs(n);
+    cout << "while ";
+    condition->print();
+    cout << " do {" << endl;
+    bodystmt->print(n + 1);
+    cout << "} ";
 }
 
 void while_stmt::evaluate()
 {
 
-	while (condition->evaluate())
-	{
-		bodystmt->evaluate();
-	}
+    while (condition->evaluate())
+    {
+        bodystmt->evaluate();
+    }
 }
 
 input_stmt::input_stmt(string name)
-	: id(name) {}
+    : id(name) {}
 
 void input_stmt::print(int n)
 {
-	output_tabs(n);
-	cout << "read " << id;
+    output_tabs(n);
+    cout << "read " << id;
 }
 
 void input_stmt::evaluate()
 {
-	float result;
+    float result;
 
-	cout << "input " << id << ":" << endl;
+    cout << "input " << id << ":" << endl;
 
-	cin >> result;
+    cin >> result;
 
-	state_float[id] = result;
+    state_float[id] = result;
 }
 
 assignment_stmt::assignment_stmt(string name, exp_node *expression)
-	: id(name), exp(expression) {}
+    : id(name), exp(expression) {}
 
 void assignment_stmt::print(int n)
 {
-	//  cout << "in assignment" << "\n";
-	output_tabs(n);
-	cout << id << " = ";
-	exp->print();
+    //  cout << "in assignment" << "\n";
+    output_tabs(n);
+    cout << id << " = ";
+    exp->print();
 }
 
 void assignment_stmt::evaluate()
 {
-	float result = exp->evaluate();
+    float result = exp->evaluate();
 
-	state_float[id] = result;
+    state_float[id] = result;
 }
 
 print_stmt::print_stmt(exp_node *myexp) : exp(myexp) {}
 
 void print_stmt::print(int n)
 {
-	output_tabs(n);
-	cout << "print ";
-	exp->print();
+    output_tabs(n);
+    cout << "print ";
+    exp->print();
 }
 
 void print_stmt::evaluate()
 {
-	cout << "output: " << exp->evaluate() << endl
-		 << endl;
+    cout << "output: " << exp->evaluate() << endl
+         << endl;
 }
 
 skip_stmt::skip_stmt() {}
@@ -422,7 +422,7 @@ skip_stmt::skip_stmt() {}
 void skip_stmt::evaluate() {}
 void skip_stmt::print(int n)
 {
-	output_tabs(n);
+    output_tabs(n);
 }
 
 sequence_stmt::sequence_stmt(statement *mystmt1, statement *mystmt2) : stmt1(mystmt1), stmt2(mystmt2)
@@ -431,15 +431,15 @@ sequence_stmt::sequence_stmt(statement *mystmt1, statement *mystmt2) : stmt1(mys
 
 void sequence_stmt::print(int n)
 {
-	stmt1->print(n);
-	cout << " ;" << endl;
-	stmt2->print(n);
+    stmt1->print(n);
+    cout << " ;" << endl;
+    stmt2->print(n);
 }
 
 void sequence_stmt::evaluate()
 {
-	stmt1->evaluate();
-	stmt2->evaluate();
+    stmt1->evaluate();
+    stmt2->evaluate();
 }
 
 expression_stmt::expression_stmt(exp_node *myexp) : exp(myexp) {}
@@ -448,22 +448,21 @@ void expression_stmt::print() {}
 
 void expression_stmt::evaluate()
 {
-	exp->evaluate();
+    exp->evaluate();
 }
 /* End statement nodes */
 
 bool test::evaluate()
 {
-	return condition->evaluate();
+    return condition->evaluate();
 }
 
 void test::print()
 {
-	condition->print();
+    condition->print();
 }
 
 test::test(cond_node *condition)
 {
-	this->condition = condition;
+    this->condition = condition;
 }
-
--- cMinus.h
+++ cMinus.h
@@ -8,314 +8,322 @@ using namespace std;
 
 enum operation
 {
-	EQ,
-	GT,
-	GE,
-	LT,
-	LE,
-	NO
+    EQ,
+    GT,
+    GE,
+    LT,
+    LE,
+    NO
 };
 
-class statement {
- public:
-  virtual void print()=0 ;
-  char* return_type (int t );
-  //virtual void evaluate() = 0;
+class statement
+{
+  public:
+    virtual void print() = 0;
+    char *return_type(int t);
+    //virtual void evaluate() = 0;
 };
 
-class exp_node: public statement {
-public:
-	// print function for pretty printing an expression
-	virtual void print() = 0;
+class exp_node : public statement
+{
+  public:
+    // print function for pretty printing an expression
+    virtual void print() = 0;
 
-	// evaluation function for a leaf, replaced for interior nodes
-	virtual float evaluate() = 0;
+    // evaluation function for a leaf, replaced for interior nodes
+    virtual float evaluate() = 0;
 };
 
 class operator_node : public exp_node
 {
-public:
-	exp_node *left;
-	exp_node *right;
+  public:
+    exp_node *left;
+    exp_node *right;
 
-	// the constructor for node links the node to its children,
-	// and stores the character representation of the operator.
-	operator_node(exp_node *L, exp_node *R);
+    // the constructor for node links the node to its children,
+    // and stores the character representation of the operator.
+    operator_node(exp_node *L, exp_node *R);
 };
 
-class arr_var : public exp_node {
-protected:
-int var_type;
-string id;
-float value;
-public:
-arr_var( string id, float value);
-arr_var(int var_type, string id, float value);
-void print();
+class arr_var : public exp_node
+{
+  protected:
+    int var_type;
+    string id;
+    float value;
+
+  public:
+    arr_var(string id, float value);
+    arr_var(int var_type, string id, float value);
+    void print();
 };
 
-class assignment_stmt: public statement{
-protected:
-int var_type;
-string id;
-exp_node *expr;
-public:
-assignment_stmt (string id, exp_node *expr);
-assignment_stmt (int var_type,string id, exp_node *expr);
-void print();
+class assignment_stmt : public statement
+{
+  protected:
+    int var_type;
+    string id;
+    exp_node *expr;
+
+  public:
+    assignment_stmt(string id, exp_node *expr);
+    assignment_stmt(int var_type, string id, exp_node *expr);
+    void print();
 };
 
-class statement_list: public statement{
- protected:
- statement *st1;
- statement *st2;
-public:
-statement_list ( statement *st1,statement *st2 );
-void print();
+class statement_list : public statement
+{
+  protected:
+    statement *st1;
+    statement *st2;
+
+  public:
+    statement_list(statement *st1, statement *st2);
+    void print();
 };
 
 class number_node : public exp_node
 {
-private:
-	float num;
+  private:
+    float num;
 
-public:
-	number_node(float value);
-	void print();
-	float evaluate();
+  public:
+    number_node(float value);
+    void print();
+    float evaluate();
 };
 
 class unary_minus_node : public exp_node
 {
-protected:
-	exp_node *exp;
+  protected:
+    exp_node *exp;
 
-public:
-	unary_minus_node(exp_node *exp);
-	void print();
-	float evaluate();
+  public:
+    unary_minus_node(exp_node *exp);
+    void print();
+    float evaluate();
 };
 
 class id_node : public exp_node
 {
-protected:
-	string id;
+  protected:
+    string id;
 
-public:
-	id_node(string value);
-	void print();
-	float evaluate();
+  public:
+    id_node(string value);
+    void print();
+    float evaluate();
 };
 
 // plus_node inherits the characteristics of node and adds its own evaluate function
 class plus_node : public operator_node
 {
-public:
-	// plus_node's constructor just uses node's constructor
-	plus_node(exp_node *L, exp_node *R);
-	void print();
-	float evaluate();
+  public:
+    // plus_node's constructor just uses node's constructor
+    plus_node(exp_node *L, exp_node *R);
+    void print();
+    float evaluate();
 };
 
 // minus_node inherits the characteristics of node and adds its own evaluate function
 class minus_node : public operator_node
 {
-public:
-	minus_node(exp_node *L, exp_node *R);
-	void print();
-	float evaluate();
+  public:
+    minus_node(exp_node *L, exp_node *R);
+    void print();
+    float evaluate();
 };
 
 // times_node inherits the characteristics of node and adds its own evaluate function
 class times_node : public operator_node
 {
-public:
-	times_node(exp_node *L, exp_node *R);
-	void print();
-	float evaluate();
+  public:
+    times_node(exp_node *L, exp_node *R);
+    void print();
+    float evaluate();
 };
 
 // divide_node inherits the characteristics of node and adds its own evaluate function
 class divide_node : public operator_node
 {
-public:
-	divide_node(exp_node *L, exp_node *R);
-	void print();
-	float evaluate();
+  public:
+    divide_node(exp_node *L, exp_node *R);
+    void print();
+    float evaluate();
 };
 
 class cond_node
 {
-public:
-	// print function for pretty printing an expression
-	virtual void print() = 0;
+  public:
+    // print function for pretty printing an expression
+    virtual void print() = 0;
 
-	// evaluation function for a leaf, replaced for interior nodes
-	virtual bool evaluate() = 0;
+    // evaluation function for a leaf, replaced for interior nodes
+    virtual bool evaluate() = 0;
 };
 
 class or_cond_node : public cond_node
 {
-private:
-	cond_node *left, *right;
+  private:
+    cond_node *left, *right;
 
-public:
-	or_cond_node(cond_node *L, cond_node *R);
-	void print();
-	bool evaluate();
+  public:
+    or_cond_node(cond_node *L, cond_node *R);
+    void print();
+    bool evaluate();
 };
 
 class and_cond_node : public cond_node
 {
-private:
-	cond_node *left, *right;
+  private:
+    cond_node *left, *right;
 
-public:
-	and_cond_node(cond_node *L, cond_node *R);
-	void print();
-	bool evaluate();
+  public:
+    and_cond_node(cond_node *L, cond_node *R);
+    void print();
+    bool evaluate();
 };
 
 class neg_cond_node : public cond_node
 {
-private:
-	cond_node *child;
+  private:
+    cond_node *child;
 
-public:
-	neg_cond_node(cond_node *child);
-	void print();
-	bool evaluate();
+  public:
+    neg_cond_node(cond_node *child);
+    void print();
+    bool evaluate();
 };
 
 class prim_cond_node : public cond_node
 {
-private:
-	operation op;
-	exp_node *left, *right;
+  private:
+    operation op;
+    exp_node *left, *right;
 
-public:
-	prim_cond_node(operation op, exp_node *L, exp_node *R);
-	void print();
-	bool evaluate();
+  public:
+    prim_cond_node(operation op, exp_node *L, exp_node *R);
+    void print();
+    bool evaluate();
 };
 
 class test
 {
-private:
-	cond_node *condition;
+  private:
+    cond_node *condition;
 
-public:
-	test(cond_node *condition);
-	void print();
-	bool evaluate();
+  public:
+    test(cond_node *condition);
+    void print();
+    bool evaluate();
 };
 
 class statement
 {
-public:
-	virtual void print(int) {}
-	virtual void evaluate() = 0;
+  public:
+    virtual void print(int) {}
+    virtual void evaluate() = 0;
 };
 
 class ife_stmt : public statement
 {
-protected:
-	test *condition;
-	statement *thenbranch, *elsebranch;
+  protected:
+    test *condition;
+    statement *thenbranch, *elsebranch;
 
-public:
-	ife_stmt(test *condition, statement *thenbranch, statement *elsebranch);
-	void print(int);
-	void evaluate();
+  public:
+    ife_stmt(test *condition, statement *thenbranch, statement *elsebranch);
+    void print(int);
+    void evaluate();
 };
 
 class while_stmt : public statement
 {
-protected:
-	test *condition;
-	statement *bodystmt;
+  protected:
+    test *condition;
+    statement *bodystmt;
 
-public:
-	while_stmt(test *condition, statement *bodystmt);
-	void print(int);
-	void evaluate();
+  public:
+    while_stmt(test *condition, statement *bodystmt);
+    void print(int);
+    void evaluate();
 };
 
 class input_stmt : public statement
 {
-protected:
-	string id;
+  protected:
+    string id;
 
-public:
-	input_stmt(string name);
-	void print(int);
-	void evaluate();
+  public:
+    input_stmt(string name);
+    void print(int);
+    void evaluate();
 };
 
 class assignment_stmt : public statement
 {
-protected:
-	string id;
-	exp_node *exp;
+  protected:
+    string id;
+    exp_node *exp;
 
-public:
-	assignment_stmt(string name, exp_node *expression);
-	void print(int);
-	void evaluate();
+  public:
+    assignment_stmt(string name, exp_node *expression);
+    void print(int);
+    void evaluate();
 };
 
 class print_stmt : public statement
 {
-protected:
-	exp_node *exp;
+  protected:
+    exp_node *exp;
 
-public:
-	print_stmt(exp_node *myexp);
-	void print(int);
-	void evaluate();
+  public:
+    print_stmt(exp_node *myexp);
+    void print(int);
+    void evaluate();
 };
 
 class skip_stmt : public statement
 {
-public:
-	skip_stmt();
-	void print(int);
-	void evaluate();
+  public:
+    skip_stmt();
+    void print(int);
+    void evaluate();
 };
 
 class sequence_stmt : public statement
 {
-protected:
-	statement *stmt1, *stmt2;
+  protected:
+    statement *stmt1, *stmt2;
 
-public:
-	sequence_stmt(statement *mystmt1, statement *mystmt2);
-	void print(int);
-	void evaluate();
+  public:
+    sequence_stmt(statement *mystmt1, statement *mystmt2);
+    void print(int);
+    void evaluate();
 };
 
 class expression_stmt : public statement
 {
-protected:
-	exp_node *exp;
+  protected:
+    exp_node *exp;
 
-public:
-	expression_stmt(exp_node *myexp);
-	void print();
-	void evaluate();
+  public:
+    expression_stmt(exp_node *myexp);
+    void print();
+    void evaluate();
 };
 
 class func_stmt : public statement
 {
-protected:
-	id_node *id;
-	id_node *param;
-	sequence_stmt *block;
+  protected:
+    id_node *id;
+    id_node *param;
+    sequence_stmt *block;
 
-public:
-	func_stmt(id_node *myID, id_node *myParam, sequence_stmt *myBlock);
-	void print();
-	void evaluate();
+  public:
+    func_stmt(id_node *myID, id_node *myParam, sequence_stmt *myBlock);
+    void print();
+    void evaluate();
 };
 
 extern map<string, float> state_float;
